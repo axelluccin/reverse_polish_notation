6ce178b11681aed0dfd042dc48c9834068266f58
sandymane <manesandy44@gmail.com>
2019-09-23 14:49:53 +0200

    Test with 2 subtraction of number

--- src/main/java/fr/lacombe/rpn/ReversePolishNotation.java
+++ src/main/java/fr/lacombe/rpn/ReversePolishNotation.java
@@ -1,26 +1,56 @@
 package fr.lacombe.rpn;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class ReversePolishNotation {
 
     private static final String SEPARATOR = " ";
 
     public int compute(String input) {
 
+        if (input.equals("")) {
+            return 0;
+        }
         if (input.length() == 1) {
             return Integer.parseInt(input);
         }
 
-        int result = 0;
-        for (String s: input.split(SEPARATOR)) {
-            if (isNumeric(s)) {
-                result += Integer.parseInt(s);
+        List<Integer> numbers = getNumbers(input);
+        List<String> operators = getOperators(input);
+        int result = numbers.get(0);
+        numbers.remove(0);
+        for (Integer number : numbers) {
+            if (operators.get(0).equals("+")) {
+                result += number;
+            } else if (operators.get(0).equals("-")) {
+                result -= number;
             }
-
-            if(s.equals("-"))
-                return 3;
         }
 
         return result;
+    }
+
+    private List<String> getOperators(String input) {
+        List<String> operators = new ArrayList<>();
+        for (String s : input.split(SEPARATOR)) {
+            if (!isNumeric(s)) {
+                operators.add(s);
+            }
+        }
+        Collections.reverse(operators);
+        return operators;
+    }
+
+    private List<Integer> getNumbers(String input) {
+        List<Integer> numbers = new ArrayList<>();
+        for (String s : input.split(SEPARATOR)) {
+            if (isNumeric(s)) {
+                numbers.add(Integer.parseInt(s));
+            }
+        }
+        return numbers;
     }
 
     private static boolean isNumeric(String str) {
