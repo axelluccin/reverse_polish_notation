3f3253e6795d16f337d46da2eb10df2a8841a1cd
sandymane <manesandy44@gmail.com>
2019-09-26 14:03:36 +0200

    refactor verification condition

--- src/main/java/fr/lacombe/rpn/InputVerification.java
+++ src/main/java/fr/lacombe/rpn/InputVerification.java
@@ -2,6 +2,12 @@ package fr.lacombe.rpn;
 
 public class InputVerification {
     public boolean verify(String input) {
-        throw new IllegalArgumentException("Input should not be empty");
+        emptyString(input);
+        throw new IllegalArgumentException("Input should not contain just a space");
+    }
+
+    private void emptyString(String input) {
+        if (input.equals(""))
+            throw new IllegalArgumentException("Input should not be empty");
     }
 }
--- src/test/java/fr/lacombe/rpn/InputVerificationTest.java
+++ src/test/java/fr/lacombe/rpn/InputVerificationTest.java
@@ -8,8 +8,9 @@ public class InputVerificationTest {
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
+
     @Test
-    public void when_input_contain_a_empty_string() {
+    public void when_input_contain_a_empty_string_then_throw_an_exception() {
         String input = "";
         InputVerification verification = new InputVerification();
         thrown.expect(IllegalArgumentException.class);
@@ -18,5 +19,13 @@ public class InputVerificationTest {
         verification.verify(input);
     }
 
+    @Test
+    public void when_input_contain_a_space_then_throw_an_exception() {
+        String input = " ";
+        InputVerification verification = new InputVerification();
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("Input should not contain just a space");
 
+        verification.verify(input);
+    }
 }
